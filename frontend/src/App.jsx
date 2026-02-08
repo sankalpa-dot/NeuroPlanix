37f35c2ac2d9efbe01c6c005ee89c1318ceb2181
sankalpa-dot <sankalpasovansamal@gmail.com>
2026-02-08 22:55:47 +0530

    chore: remove frontend files and configurations

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import AppRoutes from "./routes/AppRoutes";
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-gray-100">
-      <AppRoutes />
-    </div>
-  );
-}
--- frontend/src/components/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <div className="bg-white shadow px-6 py-3 font-bold text-blue-600">
-      NeuroPlanix
-    </div>
-  );
-}
